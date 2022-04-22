1a5c6efb824a76a5a82cc5c05f3ebf117d13fe2c
siva <siva@mmmi.sdu.dk>
2022-04-22 10:37:42 +0200

    hazel

--- AGV/AGV/HazelcastPublisherTemplate.cs
+++ AGV/AGV/HazelcastPublisherTemplate.cs
@@ -1,28 +1,16 @@
 ﻿using System;
-//using Hazelcast.Config;
+using System.Threading.Tasks;
+using Hazelcast;
+using Hazelcast.Client;
 using Hazelcast.Core;
-//using Hazelcast.Client;
 
 
-namespace ST4_ImplementationExamples
+namespace AGV
 {
     public class HazelcastClientTemplate
-    {/*
-       static void Communicate ()  { 
-           // get Hazelcast instance
-       HazelcastInstance hz = getHazelcastInstance();
-       // publish to topic, creates a new if it doesn't exist
-       ITopic<String> topic = hz.getTopic("ExampleTopic");
-       topic.publish("Example data");
-       Console.WriteLine("Message published by topic publisher HazelcastTemplatePublisher");
-    }
-    private static HazelcastInstance getHazelcastInstance(){
-        //hazelcast address for the clients
-        var clientConfig = new ClientConfig();
-        // don't know if port can be used here
-        clientConfig.GetNetworkConfig().AddAddress("127.0.0.1:5701");
-        return HazelcastClient.newHazelcastClient(clientConfig);
-    }
-   */
+    {
+        // private HazelcastClient hz;
+        // hz
+
     }
 }
--- AGV/AGV/Program.cs
+++ AGV/AGV/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Hazelcast.Client;
+using Hazelcast.Core;
 
 namespace AGV
 {
@@ -6,12 +8,29 @@ namespace AGV
     {
         static void Main(string[] args)
         {
+            
+            // Start the Hazelcast Client and connect to an already running Hazelcast Cluster on 127.0.0.1
+            IHazelcastInstance hz = HazelcastClient.NewHazelcastClient();
+            // Get the Distributed Map from Cluster.
+            var topic = hz.GetTopic<string>("topic");
+            topic.GetName();
+            // var map = hz.GetMap<string,int>("theMap");
+            // // Standard Put and Get.
+            //  map.Put("key", 1);
+            //  map.Get("key");
+            //  //Concurrent Map methods, optimistic updating
+            //  map.PutIfAbsent("somekey", 1);
+            //  map.Replace("key", 1, 2);
+             // Shutdown this Hazelcast Client
+            hz.Shutdown();
+            
+            
             //REST
-            REST rest = new REST();
-            _ = rest.RunExample();
-           
-
-            Console.ReadKey();
+            // REST rest = new REST();
+            // _ = rest.RunExample();
+            //
+            //
+            // Console.ReadKey();
         }
     }
 }
