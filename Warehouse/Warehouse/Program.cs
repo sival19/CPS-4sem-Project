780f8a97ac90c8f8e88128ab47d325ffdf5f18a8
Maria <mariahansen.mmh@gmail.com>
2022-04-29 10:58:43 +0200

    Hazelcast working with "HelloSOAP" message

--- Warehouse/Warehouse/Program.cs
+++ Warehouse/Warehouse/Program.cs
@@ -1,17 +1,43 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+using Hazelcast;
+using WarehouseService;
 
 namespace Warehouse
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task PublishTopic(string tp, string message)
         {
+            await using var client = await HazelcastClientFactory.StartNewClientAsync();
+            await using var topic = await client.GetTopicAsync<String>(tp);
+            await topic.PublishAsync(message);
+        }
+         static async Task startSOAP()
+                {
+                    //SOAP
+                    //SOAP soap = new SOAP();
+                    //_ = soap.RunExample();
 
-            //SOAP
-            SOAP soap = new SOAP();
-            _ = soap.RunExample();
+                    //Console.ReadKey();
+                }
+        
 
-            Console.ReadKey();
+
+        static async Task Main(string[] args)
+        {
+            // make a SOAP request here
+            string s = "HelloSOAP";
+            
+            // request.PutOperation();
+
+            
+            while (true)
+            {
+                await PublishTopic("WarehousePubTopic", s);
+            }
         }
     }
 }
+
