e845e1bd99f8e08972b47deece2065034ad30f64
jepla20 <jepla20@student.sdu.dk>
2022-04-29 09:57:25 +0200

    Test

--- ProductionManagementSystem/src/main/java/dk/sdu/sem4/Main.java
+++ ProductionManagementSystem/src/main/java/dk/sdu/sem4/Main.java
@@ -29,7 +29,7 @@ public class Main {
                         System.out.println("TODO");
                         break;
                     case "3":
-                        System.out.println("Why would you quit ME?!");
+                        System.out.println("Why would you quit");
                         running = false;
                         System.exit(0);
                         break;
