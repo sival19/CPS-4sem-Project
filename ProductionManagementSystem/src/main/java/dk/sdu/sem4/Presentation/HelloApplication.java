13878e158e1bd1acb881cc324e33b10dfa99a69c
Maria <mariahansen.mmh@gmail.com>
2022-05-11 09:51:40 +0200

    Gui AGV

--- ProductionManagementSystem/src/main/java/dk/sdu/sem4/Presentation/HelloApplication.java
+++ ProductionManagementSystem/src/main/java/dk/sdu/sem4/Presentation/HelloApplication.java
@@ -16,8 +16,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 240);
-        stage.setTitle("hello");
+        Scene scene = new Scene(fxmlLoader.load(), 1066, 574);
+        stage.setTitle("Drone Production System");
         stage.setScene(scene);
         stage.show();
 
--- ProductionManagementSystem/src/main/java/module-info.java
+++ ProductionManagementSystem/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module ProductionManagementSystem {
     requires javafx.fxml;
     requires hazelcast.all;
     requires java.se;
+    requires org.json;
 
     opens dk.sdu.sem4.Presentation to javafx.fxml;
     exports dk.sdu.sem4.Presentation;
